fffd2ffb83904fe9d2a0b93e318a6724b4ed20dc
z1026425556 <z53137473@126.com>
2021-11-04 18:55:35 +0800

    第一次提交代码

---
+++ src/main/java/com/example/demo2/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String demo(){
+        return "hello springboot!";
+    }
+
+}
